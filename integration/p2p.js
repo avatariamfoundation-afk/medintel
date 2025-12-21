0f7985f2629bde8433396cddc493a15c4b90b01d
Aethera BioSync <avatariamfoundation@gmail.com>
2025-12-22 00:27:37 +0200

    Update p2p.js

--- integration/p2p.js
+++ integration/p2p.js
@@ -1,1 +1,20 @@
+const Libp2p = require('libp2p');
+const TCP = require('libp2p-tcp');
+const { NOISE } = require('libp2p-noise');
+const MPLEX = require('libp2p-mplex');
 
+async function startP2PNode() {
+  const libp2p = await Libp2p.create({
+    addresses: { listen: ['/ip4/0.0.0.0/tcp/0'] },
+    modules: {
+      transport: [TCP],
+      connEncryption: [NOISE],
+      streamMuxer: [MPLEX]
+    }
+  });
+  await libp2p.start();
+  console.log('P2P node started on port', libp2p.multiaddrs[0].toString());
+  return libp2p;
+}
+
+module.exports = { startP2PNode };
