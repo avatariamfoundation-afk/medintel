d85ed01e61223ff736922fda4087b89660d8d5d9
Aethera BioSync <avatariamfoundation@gmail.com>
2025-12-21 23:33:47 +0200

    Update .hardhat.config.js

--- .hardhat.config.js
+++ .hardhat.config.js
@@ -1,1 +1,13 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    testnet: {
+      url: process.env.BNB_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  // Future: Add hardhat-deploy for easier scaling
+};
